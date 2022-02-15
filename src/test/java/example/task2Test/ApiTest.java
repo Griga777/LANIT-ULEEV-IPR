36f954507b0cf659221846caffaeacf239979259
Uleev <uleev777@yandex.ru>
2022-02-16 00:39:02 +0400

    2 задание в процессе

--- src/test/java/example/task2Test/ApiTest.java
+++ src/test/java/example/task2Test/ApiTest.java
@@ -3,39 +3,25 @@ package example.task2Test;
 import example.task2.trello.boards.Board;
 import example.task2.trello.cards.Card;
 import example.task2.trello.lists.List;
-import example.task2Test.utils.RestWrapper;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.core.StringContains.containsString;
 
 public class ApiTest {
-    private static RestWrapper api;
     private static final String BASE_URL = "https://api.trello.com";
-    private RequestSpecification requestSpec;
-    private ResponseSpecification responseSpec;
 
     @BeforeClass
     public static void prepareRequest() {
-//        api = RestWrapper.loginAs("ca92798ed22edd169506048c77755169",
-//                                  "44962edeb80408666c4dd3ed952a463ad9b4555852c6f779c91f1a9536750777");
-
-//        System.getProperties().load(ClassLoader.getSystemResourceAsStream("my.properties"));
-
-        RequestSpecification requestSpec = new RequestSpecBuilder()
-                .setBaseUri("https://api.trello.com")
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setBaseUri(BASE_URL)
                 .addQueryParam("key", "ca92798ed22edd169506048c77755169")
                 .addQueryParam("token", "44962edeb80408666c4dd3ed952a463ad9b4555852c6f779c91f1a9536750777")
                 .setAccept(ContentType.JSON)
@@ -43,15 +29,6 @@ public class ApiTest {
                 .log(LogDetail.ALL)
                 .build();
         RestAssured.filters(new ResponseLoggingFilter());
-        RestAssured.requestSpecification = requestSpec;
-    }
-
-    public static void prepareResponse() {
-        ResponseSpecification responseSpec = new ResponseSpecBuilder()
-                .expectStatusCode(200)
-                .expectBody(containsString("success"))
-                .build();
-        RestAssured.responseSpecification = responseSpec;
     }
 
     @Test
