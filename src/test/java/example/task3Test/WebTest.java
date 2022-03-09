2ac501263a0cadab5bf12c65d22d7a0f8332a277
Uleev <uleev777@yandex.ru>
2022-03-09 14:27:02 +0400

    3 задание в процессе

--- src/test/java/example/task3Test/WebTest.java
+++ src/test/java/example/task3Test/WebTest.java
@@ -82,18 +82,10 @@ public class WebTest {
         String checkitemName1 = "Понять протокол HTTP";
         String checkitemName2 = "Выучить методы запросов";
 
-        params = driver.findElement(By.xpath("//span[contains(text(), '" + checkitemName1 + "')]//ancestor-or-self::div[contains(@class, 'checklist-item no-assignee no-due')]"));
-        if (params.getText().contains(" checklist-item-state-complete")) {
-            System.out.println("Пункт " + checkitemName1 + " выполнен");
-        } else {
-            System.out.println("Пункт " + checkitemName1 + " не выполнен");
-        }
-        params = driver.findElement(By.xpath("//span[contains(text(), '" + checkitemName2 + "')]//ancestor-or-self::div[contains(@class, 'checklist-item no-assignee no-due')]"));
-        if (params.getText().contains(" checklist-item-state-complete")) {
-            System.out.println("Пункт " + checkitemName2 + " выполнен");
-        } else {
-            System.out.println("Пункт " + checkitemName2 + " не выполнен");
-        }
+        driver.findElement(By.xpath("//span[contains(text(), '" + checkitemName1 + "')]//ancestor-or-self::div[contains(@class, 'checklist-item no-assignee no-due checklist-item-state-complete')]")).isDisplayed();
+        System.out.println("Пункт " + checkitemName1 + " выполнен");
+        driver.findElement(By.xpath("//span[contains(text(), '" + checkitemName2 + "')]//ancestor-or-self::div[contains(@class, 'checklist-item no-assignee no-due checklist-item-state-complete')]")).isDisplayed();
+        System.out.println("Пункт " + checkitemName2 + " выполнен");
         params = driver.findElement(By.xpath("//span[@class = 'checklist-progress-percentage js-checklist-progress-percent']"));
         if (params.getText().contains("100%")) {
             System.out.println("Чек-лист выполнен на 100%");
